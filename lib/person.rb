918f6f8918f65b37cf007d5ee3f8888df7b336e2
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-17 19:03:46 +0300

    initial

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # your code goes here
+class Person
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+  
+    def initialize(name)
+      @name = name
+      @bank_account = 25
+      @happiness = 8
+      @hygiene = 8
+    end
